10ef44c1b50506920daee3657c2b013f7efaa387
KGG-ai <2305101010204@paruluniversity.ac.in>
2025-08-16 13:04:12 +0530

    Ready for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./Chat.js";
 import Chat from './Chat';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Chat/>}></Route>
+        <Route path="/" element={<Chat />} />
       </Routes>
     </Router>
   );
