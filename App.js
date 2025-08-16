b603150a2b941902b924d8a6ad127953532aa380
Komal Rajput <komalr635122@gmail.com>
2025-08-16 12:56:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Chat from './Chat';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Chat/>}></Route>
       </Routes>
