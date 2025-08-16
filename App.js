ecb9d6c4b282764b700ef3b722223ac03e87b605
Komal Rajput <komalr635122@gmail.com>
2025-08-16 12:41:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,42 +1,19 @@
-import React, { useState, useEffect } from "react";
-import Orb from "./orb";
-import Subtitle from "./subtitle";
-import VoiceCapture from "./voicecapture";
-import "./orb.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./Chat.js";
+import Chat from './Chat';
 
 function App() {
-  const [subtitle, setSubtitle] = useState("Listening...");
-
-  const handleVoiceCommand = async (text) => {
-    console.log("Received:", text);
-    setSubtitle(`You said: ${text}`);
-
-    try {
-      await fetch("http://localhost:5000/speak", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text }),
-      });
-    } catch (err) {
-      console.error("Failed to send to backend:", err);
-    }
-  };
-
-
   return (
-    <div className="veda-container">
-      <h1>VEDA AI</h1>
-      <Orb />
-      <Subtitle text={subtitle} />
-      <VoiceCapture onCommand={handleVoiceCommand} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Chat/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
 
 
 
